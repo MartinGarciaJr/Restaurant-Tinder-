9071680d16d9d1a9185df08e15e7d1ecd61d803d
MartinGarciaJr <87857432+MartinGarciaJr@users.noreply.github.com>
2022-03-13 21:30:39 -0400

    Add files via upload

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,99 @@
+import {Component} from 'react'
+import {Switch, Route, Redirect, Link} from 'react-router-dom'
+import Login from '../Login/Login'
+import Register from '../Register/Register'
+import Home from '../Home/Home'
+import {addBusinesses, addFavorites, addToken, deleteUser,fetchFavorites } from '../../Redux/actionCreators'
+import {connect} from 'react-redux'
+import {withRouter} from 'react-router-dom'
+import Footer from '../FooterComponent'
+import delish from '../Shared/images/delish.jpeg'
+import RenderRestaurant from '../RenderRestaurants'
+
+
+import { Navbar, NavbarBrand, } from 'reactstrap';
+
+import Favorites from '../Favorites'
+import Invite from '../InviteComponent'
+    
+
+const mapStateToProps = state => {
+    return {
+        token: state.token,
+        user: state.user,
+        businesses: state.businesses,
+        favorites: state.favorites
+    }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+    addToken: () => { dispatch(addToken()) },
+    deleteUser: () => { dispatch(deleteUser())},
+    addBusinesses: () => { dispatch(addBusinesses)},
+    addFavorites: () => { dispatch(addFavorites)},
+    fetchFavorites: () => { dispatch(addFavorites())}
+    
+
+});
+
+class Main extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    componentDidMount() {
+       this.props.fetchFavorites(this.props.token.token)
+    }
+
+    handleLogout = () => {
+        this.props.addToken("")
+        this.props.deleteUser()
+    }
+
+    
+
+    render(){
+
+        return(
+            <div>
+                {this.props.token.token !== undefined ?
+                        <div>
+                            <Navbar style={{backgroundColor: '#8E0009'}}>
+                            <NavbarBrand className='mr-auto' href="/">
+                                <img src={delish} height="45" width="110" alt="Delish logo"/>
+                            </NavbarBrand>
+                            <Link to='/home'> Home | </Link>
+                            <Link to='/favorites'> My favorites | </Link>
+                            <Link to='/invite'> Invite friends | </Link>
+                            <Link to='/login' onClick={this.handleLogout}>  logout</Link> 
+                            <Redirect to='/home'/>
+                            </Navbar>
+
+                        </div>  
+                    :
+                        <Navbar style={{backgroundColor: '#8E0009'}}>
+                            <NavbarBrand className='mr-auto' href="/">
+                                <img src={delish} height="45" width="110" alt="Delish logo"/>
+                            </NavbarBrand>
+                           <Link to='/login'>  Home |  </Link> 
+                           <Link to='/abouts'>  AboutUs | </Link> 
+                        </Navbar>
+                        
+                }
+                <Switch>
+                    <Route path='/login' component={() => <Login/>}/>
+                    <Route path='/register'component={() => <Register/>}/>
+                    <Route path='/home' component={this.props.token.token !== undefined ? () => <Home businesses={this.props.businesses}token= {this.props.token.token}/> : null}/>
+                    <Route path='/favorites' component={ () => <Favorites favorites={this.props.favorites}businesses={this.props.businesses} token= {this.props.token.token} user ={this.props.user} />} />
+                    <Route path='/restaurants' component={ () => <RenderRestaurant businesses={this.props.businesses} token= {this.props.token.token} />} />
+                    <Route path='/invite' component={ () => <Invite/>} />
+                    <Redirect to='/login'/>
+                    
+                </Switch>
+                <Footer/>
+            </div>
+        )
+    }
+} 
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
